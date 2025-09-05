babfe042cbf443cdcd0ea2c57ff88f8d4bdfc61f
HimakarC <116370824+HimakarC@users.noreply.github.com>
2025-09-06 00:55:21 +0530

    Update urls.py

--- hello/urls.py
+++ hello/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home, name="home"), path('bring',views.bring, name="bring"), path('health/', health_check, name='health_check'), path('', views.my_view_1, name='service1_view'),
-    path('', views.my_view_2, name='service2_view'),
+    path('',views.home, name="home"),
+    path('bring',views.bring, name="bring")
 ]
+
 
