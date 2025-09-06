836fcc5c1aeb69605b428bc48218607bb3be9774
HimakarC <116370824+HimakarC@users.noreply.github.com>
2025-09-06 13:57:54 +0530

    Update urls.py

--- hello/urls.py
+++ hello/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name="home"),
-    path('bring',views.bring, name="bring")
+    path('bring',views.bring, name="bring"),
+    path('service1',views.home, name="home"),
+    path('service2',views.home, name="home")
 ]
+
 
 
